95230ff9d498e9485868a539b03f43ec391b9d0e
suzukiplan <suzukiplan.ys@gmail.com>
2023-07-26 18:09:56 +0900

    LED, RST を GPIO で繋ぐ形に修正

--- src/app.cpp
+++ src/app.cpp
@@ -2,13 +2,17 @@
 
 #define PIN_CS GPIO_NUM_1
 #define PIN_DC GPIO_NUM_3
-#define PIN_SDA GPIO_NUM_13
-#define PIN_SCL GPIO_NUM_15
+#define PIN_RST GPIO_NUM_5
+#define PIN_LED GPIO_NUM_7
+#define PIN_MOSI GPIO_NUM_13
+#define PIN_SCLK GPIO_NUM_15
 
-static Adafruit_ILI9341 lcd(PIN_CS, PIN_DC, PIN_SDA, PIN_SCL);
+static Adafruit_ILI9341 lcd(PIN_CS, PIN_DC, PIN_MOSI, PIN_SCLK, PIN_RST);
 
 void setup()
 {
+    pinMode(PIN_LED, OUTPUT);
+    digitalWrite(PIN_LED, HIGH);
     lcd.begin();
     lcd.startWrite();
     lcd.setRotation(2);
@@ -17,9 +21,9 @@ void setup()
     int16_t x, y;
     uint16_t w, h;
     lcd.setTextSize(3);
+    lcd.setTextColor(0xFFFF, 0x0000);
     lcd.getTextBounds(hello, 0, 0, &x, &y, &w, &h);
     lcd.setCursor(lcd.width() / 2 - w / 2, lcd.height() / 2 - h / 2);
-    lcd.setTextColor(0xFFFF, 0x0000);
     lcd.print(hello);
     lcd.endWrite();
 }
